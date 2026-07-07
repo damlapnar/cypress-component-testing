fd70bf46b52927b605d06ce7471b08ae93d90b12
Elif Damla Pinar <damlapnar1@gmail.com>
2026-07-07 11:48:03 -0500

    test: expand checkout suite to 7 tests

--- cypress/e2e/checkout.cy.ts
+++ cypress/e2e/checkout.cy.ts
@@ -21,4 +21,46 @@ describe('Checkout Flow', () => {
     cy.get('[data-test="continue"]').click();
     cy.get('[data-test="error"]').should('contain', 'First Name is required');
   });
+
+  it('should show error when last name is missing', () => {
+    cy.get('[data-test="checkout"]').click();
+    cy.get('[data-test="firstName"]').type('Damla');
+    cy.get('[data-test="continue"]').click();
+    cy.get('[data-test="error"]').should('contain', 'Last Name is required');
+  });
+
+  it('should show error when postal code is missing', () => {
+    cy.get('[data-test="checkout"]').click();
+    cy.get('[data-test="firstName"]').type('Damla');
+    cy.get('[data-test="lastName"]').type('Pinar');
+    cy.get('[data-test="continue"]').click();
+    cy.get('[data-test="error"]').should('contain', 'Postal Code is required');
+  });
+
+  it('should show order summary on step two', () => {
+    cy.get('[data-test="checkout"]').click();
+    cy.get('[data-test="firstName"]').type('Damla');
+    cy.get('[data-test="lastName"]').type('Pinar');
+    cy.get('[data-test="postalCode"]').type('10001');
+    cy.get('[data-test="continue"]').click();
+    cy.url().should('include', 'checkout-step-two');
+    cy.get('.cart_item').should('be.visible');
+    cy.get('.summary_total_label').should('be.visible');
+  });
+
+  it('should cancel from step one and return to cart', () => {
+    cy.get('[data-test="checkout"]').click();
+    cy.get('[data-test="cancel"]').click();
+    cy.url().should('include', 'cart');
+  });
+
+  it('should cancel from step two and return to inventory', () => {
+    cy.get('[data-test="checkout"]').click();
+    cy.get('[data-test="firstName"]').type('Damla');
+    cy.get('[data-test="lastName"]').type('Pinar');
+    cy.get('[data-test="postalCode"]').type('10001');
+    cy.get('[data-test="continue"]').click();
+    cy.get('[data-test="cancel"]').click();
+    cy.url().should('include', 'inventory');
+  });
 });
